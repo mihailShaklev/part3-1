6985d4e75b317a74eefc2ed11deda9ce3bc85faa
mixxo <mshaklev@gmail.com>
2024-05-08 16:29:03 +0300

    task 3.6

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { response } = require('express')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     { 
@@ -24,6 +25,12 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+const generateId = () => {
+    
+    let max = 1000000000
+    return Math.floor(Math.random() * max);
+  }
 
 app.get('/info', (request, response) => {
 
@@ -55,6 +62,46 @@ app.get('/api/persons/:id', (request, response) => {
         response.status("404").send('Person not found.')
     }
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+    
+    let id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+
+})
+
+app.post('/api/persons', (request, response) => {
+
+    const body = request.body
+    const name = request.body.name
+    
+    if (!body.name){
+        return response.status(400).json({"error":"Missing person`s name."})
+    }
+
+    if (!body.number){
+        return response.status(400).json({"error":"Missing person`s number."})
+    }
+
+    let nameInDB = persons.filter(person => person.name === name)
+
+    if (nameInDB.length > 0){
+        return response.status(400).json({"error":"Name must be unique."})
+    }
+
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number,
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
